f2305bb98f98243496a607f1e76fa063b9b8d45e
DaiChiora <111251517+DaiChiora@users.noreply.github.com>
2023-03-05 17:04:44 -0300

    backend

--- BackEnd/src/main/java/com/portfoliodc/Daiana/Chiora/Controller/PersonaController.java
+++ BackEnd/src/main/java/com/portfoliodc/Daiana/Chiora/Controller/PersonaController.java
@@ -10,30 +10,33 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/personas")
+
 public class PersonaController {
     @Autowired IPersonaService IpersonaService;
-    
-    @GetMapping("personas/traer")
+  
+    @GetMapping("/traer")
     public List<Persona> getPersona() {
         return IpersonaService.getPersona();
     }
     
-    @PostMapping("personas/crear")
+    @PostMapping("/crear")
     public String createPersona(@RequestBody Persona persona) {
         IpersonaService.savePersona(persona);
         return "La persona fue creada correctamente";
     }
     
-    @DeleteMapping("personas/borrar/{id}")
+    @DeleteMapping("/borrar/{id}")
     public String deletePersona(@PathVariable Long id) {
         IpersonaService.deletePersona(id);
         return "La persona fue eliminada correctamente";
     }
-    @PutMapping ("personas/editar/{id}")
+    @PutMapping ("/editar/{id}")
     public Persona editPersona(@PathVariable Long id,
                               @RequestParam("nombre") String nuevoNombre,
                               @RequestParam("apellido") String nuevoApellido,
--- BackEnd/src/main/java/com/portfoliodc/Daiana/Chiora/Interface/IPersonaService.java
+++ BackEnd/src/main/java/com/portfoliodc/Daiana/Chiora/Interface/IPersonaService.java
@@ -4,12 +4,19 @@ import com.portfoliodc.Daiana.Chiora.Entity.Persona;
 import java.util.List;
 
 public interface IPersonaService {
+    //Traer una lista de personas
     
     public List<Persona> getPersona();
     
+    //Guardar un objeto de tipo Persona
+    
     public void savePersona(Persona persona);
     
+    //Eliminar un objeto
+    
     public void deletePersona(Long id);
+    
+    //Buscar una persona por id
     
     public Persona findPersona(Long id);
     
