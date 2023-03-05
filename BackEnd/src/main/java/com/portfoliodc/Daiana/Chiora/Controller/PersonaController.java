8cade8299f4f08c114c070f7b25e98f6c591ce20
DaiChiora <111251517+DaiChiora@users.noreply.github.com>
2023-03-05 18:22:33 -0300

    corrijo backend

--- BackEnd/src/main/java/com/portfoliodc/Daiana/Chiora/Controller/PersonaController.java
+++ BackEnd/src/main/java/com/portfoliodc/Daiana/Chiora/Controller/PersonaController.java
@@ -4,6 +4,7 @@ import com.portfoliodc.Daiana.Chiora.Entity.Persona;
 import com.portfoliodc.Daiana.Chiora.Interface.IPersonaService;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin
 @RestController
 @RequestMapping("/personas")
 
